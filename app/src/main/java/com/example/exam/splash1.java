a8535f4278bf266fecd74b7657d5a79dbf75c284
user <kivin2001@gmail.com>
2022-05-13 11:11:31 +0900

    splash

--- app/src/main/java/com/example/exam/splash1.java
+++ app/src/main/java/com/example/exam/splash1.java
@@ -6,20 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+public class splash1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(splash1.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         }, 3*1000);
+
+
     }
 }
