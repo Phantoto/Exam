df174895310a9e89ae6a3c4e9d3678dcaab4d0cf
user <kivin2001@gmail.com>
2022-05-19 06:33:48 +0900

    made a cringe

---
+++ app/src/main/java/com/example/exam/OnBoardingScreen1.java
@@ -0,0 +1,14 @@
+package com.example.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_screen1);
+    }
+}
---
+++ app/src/main/java/com/example/exam/OnBoardingScreen2.java
@@ -0,0 +1,14 @@
+package com.example.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingScreen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_screen2);
+    }
+}
